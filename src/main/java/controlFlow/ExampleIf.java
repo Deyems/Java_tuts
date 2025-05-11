b2de25f04d16e9081d27ef89061a01388d95859d
Deyems <adeyemisola11@gmail.com>
2025-05-11 21:50:05 +0100

    updated to remove warnings

--- src/main/java/controlFlow/ExampleIf.java
+++ src/main/java/controlFlow/ExampleIf.java
@@ -3,11 +3,6 @@ package controlFlow;
 public class ExampleIf {
     public static void main(String[] args) {
         int input = 1;
-        if(input != 0){
-            System.out.println("2 divided by " + input + " equals " + (2 / input));
-        }else{
-            System.out.println("Input is less/equal to ZERO!");
-        }
-        return;
+        System.out.println("2 divided by " + input + " equals " + (2 / input));
     }
 }
